245dbdd2db65aac7edcd863d6b77f403eb2e9453
YolisJC <a01285390@tec.mx>
2025-02-21 18:04:19 -0600

    w

--- demo/src/app/pages/dashboard/dashboard.component.ts
+++ demo/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/header/header.component'; 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
   standalone: true,
-  imports: [HeaderComponent]
+  imports: []
 })
 export class DashboardComponent { }
--- demo/src/app/pages/profile/profile.component.ts
+++ demo/src/app/pages/profile/profile.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/header/header.component'; 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
   standalone: true,
-  imports: [HeaderComponent]
+  imports: []
 })
 export class ProfileComponent { }
 
